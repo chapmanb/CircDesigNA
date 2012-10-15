78933d6a6a7a9f6404169e3d7e906d952fd1552a
chapmanb <chapmanb@50mail.com>
2012-10-15 12:09:36 -0400

    Provide stable version 0.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.chapmanb/circdesigna "0.1.0-SNAPSHOT"
+(defproject org.clojars.chapmanb/circdesigna "0.0.1"
   :description "Synthetic biology design tool with support for extensible sequence constraints"
   :url "https://github.com/taifunbrowser/CircDesigNA"
   :dependencies [[org.clojure/clojure "1.4.0"]
